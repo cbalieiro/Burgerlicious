13a3db1d1f4a794295fc560fe8464e0c981870bc
Beatriz Penalva <biapenalva@gmail.com>
2021-02-17 15:02:54 -0300

    incorporando os componentes na home do hall

--- src/pages/Hall/index.jsx
+++ src/pages/Hall/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Header from "../../components/header";
+import Footer from "../../components/footer";
 
-const Hall = () => (
-  <div>
-    <h1>Aqui só tem garçons</h1>
-    <p>
-      <Link to="/Kitchen"> VOLTE PARA O LOGIN </Link>
-    </p>
-  </div>
-);
+const Hall = () => {
+
+  const nameLS = JSON.parse(localStorage.getItem('currentUser'));
+
+  return (
+    <>
+      <Header
+        role={'Attendant'}
+        name={nameLS.name}
+      />
+
+      <main className="home">
+        <h1>Aqui aparecerá os outros componentes</h1>
+      </main>
+
+      <Footer />
+    </>
+  )
+};
 
 export default Hall;
--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import Header from '../../components/headerKitchen'
+import React from "react";
+import Header from "../../components/header";
 import Footer from "../../components/footer";
 
 const Kitchen = () => {
-// const nameLS = JSON.parse(localStorage.getItem('currentUser'));
-// const {name, role} = nameLS;
-// // function result () {
-// //   if (role === "kitchen"){
-// //     return <div>
-// //       <Header 
-// //         role={'Chef'}
-// //         name={`${name}`}
-// //       />
-// //       <h1>Aqui só tem Chefs</h1>
-// //     </div>
-// //   }
-// //   else {
-// //     return alert("bocó")
-// //   }
-  
-// // };
-// //   return result()
+  // const nameLS = JSON.parse(localStorage.getItem('currentUser'));
+  // const {name, role} = nameLS;
+  // // function result () {
+  // //   if (role === "kitchen"){
+  // //     return <div>
+  // //       <Header 
+  // //         role={'Chef'}
+  // //         name={`${name}`}
+  // //       />
+  // //       <h1>Aqui só tem Chefs</h1>
+  // //     </div>
+  // //   }
+  // //   else {
+  // //     return alert("bocó")
+  // //   }
 
-const nameLS = JSON.parse(localStorage.getItem('currentUser'));
+  // // };
+  // //   return result()
 
-return (
-  <>
-    <Header
-      role={'Chef'}
-      name={nameLS.name}
-    />
-    <main className="main-kitchen">
-      <h1>Aqui aparecerá os pedidos</h1>
-    </main>
-    <Footer />
-  </>
-)
+  const nameLS = JSON.parse(localStorage.getItem('currentUser'));
 
-  };
+  return (
+    <>
+      <Header
+        role={'Chef'}
+        name={nameLS.name}
+      />
+
+      <main className="home">
+        <h1>Aqui aparecerá os pedidos</h1>
+      </main>
+      
+      <Footer />
+    </>
+  )
+};
 
 export default Kitchen;
