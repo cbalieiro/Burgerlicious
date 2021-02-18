a81dc7fa5c625e0bbba7b7c4fc189b6277fcb684
cbalieiro <bmoliveira.camila@gmail.com>
2021-02-18 11:10:12 -0300

    removendo mocky

--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,74 +1,44 @@
-import React from "react"
-// import React, { useState } from "react";
-// import RequestOptions from "../../components/object/requestOptions";
+import React from 'react';
+import React, { useState } from 'react';
+import RequestOptions from '../../components/object/requestOptions';
 // import AllModelsObject from "../../components/object/models";
-// import CallAPI from "../../services/api";
-import HeaderKitchen from "../../components/headerKitchen";
-import CardsKitchen from "../../components/cardsKitchen";
+import CallAPI from '../../services/api';
+import HeaderKitchen from '../../components/headerKitchen';
+import CardsKitchen from '../../components/cardsKitchen';
 
 // const orderData = AllModelsObject.ordersList;
 
-const teste = {
-  id: 1,
-  clientName: "Camila",
-  userId: 296,
-  table: "007",
-  status: "Pending",
-  processedAt: "",
-  createdAt: "",
-  updatedAt: "",
-  products: [
-    {
-      id: 1,
-      name: "Café americano",
-      flavor: null,
-      complement: null,
-      qtd: 2,
-    },
-  ],
-};
-
 const Kitchen = () => {
-  // const [orderDB, setorderDB] = useState([]);
-  const nameLS = JSON.parse(localStorage.getItem("currentUser"));
+  const [orderDB, setOrderDB] = useState(0);
+  const nameLS = JSON.parse(localStorage.getItem('currentUser'));
   const { name, role, token } = nameLS;
-  console.log(token);
-  const { table, clientName, createdAt } = teste;
 
-  // const orderList = () => {
-  //   const method = RequestOptions.getAndDelete("GET", token);
-  //   const URL = "https://lab-api-bq.herokuapp.com/orders  ";
+  const orderList = () => {
+    const method = RequestOptions.getAndDelete('GET', token);
+    const URL = 'https://lab-api-bq.herokuapp.com/orders  ';
 
-  //   CallAPI(URL, method)
-  //   .then((json) => {
-  //     console.log(json);
-  //     setorderDB([...orderDB,json])
-  //     // const orders = json;
-  //     // orders.forEach((item) => {
-  //     // console.log(item)
-  //     // setOrderList(item)});
-  //   });
-    
-  // };
-
-  // orderList();
+    CallAPI(URL, method).then((json) => {
+      setOrderDB([...orderDB, json]);
+      // const orders = json;
+      // orders.forEach((item) => {
+      // console.log(item)
+      // setOrderList(item)});
+    });
+  };
 
   function result() {
-    if (role === "kitchen") {
-      
-      return (
-        // orderDB.forEach((item) => {console.log(item))
-        <div>
-          <HeaderKitchen chef={`${name}`} />
-          <CardsKitchen
-            table={table}
-            clientName={clientName}
-            createdAt={createdAt}
-          />
-        </div>
-      );
+    if (role === 'kitchen') {
+      return orderDB.forEach((item) => {
+        console.log(item);
+        return (
+          <>
+            <HeaderKitchen />
+            <CardsKitchen />
+          </>
+        );
+      });
     } else {
-      return alert("Você não possui acesso a esse Módulo");
+      return alert('Você não possui acesso a esse Módulo');
     }
   }
 
