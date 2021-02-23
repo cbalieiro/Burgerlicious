586383afc8de8ee876c5198b958875544f0172ab
Beatriz Penalva <biapenalva@gmail.com>
2021-02-23 14:37:39 -0300

    criando objeto com o id e quantidade de produtos

--- src/components/menudetails.js
+++ src/components/menudetails.js
@@ -26,24 +26,25 @@ const MenuItems = ({ item }) => {
         const allProducts = data;
 
         setSnacksList(allProducts.filter((item) => item.name.includes("Misto")));
-        setCoffeeList(allProducts.filter((item) => item.name.includes("Café" || "Suco")));
+        setCoffeeList(allProducts.filter((item) => item.name.includes("Café", "Suco")));
         setBurgerList(allProducts.filter((item) => item.name.includes("Hambúrguer")));
-        setDrinksList(allProducts.filter((item) => item.name.includes("Água" || "Refrigerante")));
-        setSidesList(allProducts.filter((item) => item.name.includes("Batata" || "Cebola")));
+        setDrinksList(allProducts.filter((item) => item.name.includes("Água", "Refrigerante")));
+        setSidesList(allProducts.filter((item) => item.name.includes("Batata", "Cebola")));
     }, [data])
 
+    const handleClick = (items) => {
+        for (const property in items) {
+            createItemObject(property, items[property]);
+        }
+    }
 
-    //fazer uma função anterior ao handleClick
-    const handleClick = (count) => {
-        console.log(count)
-        const product = document.querySelector(".product").innerText;
-        const cost = document.querySelector(".price").innerText;
-        // const qt = document.querySelector(".quantity-field").innerText;
-
+    const createItemObject = (code, count) => {
+        //montar igual a API pede // como pegar o name e o price
         const itemsData = {
-            name: product,
+            id: code,
+            // name: product,
             quantity: count,
-            price: cost,
+            // price: cost,
         }
 
         addItem(itemsData);
@@ -55,64 +56,65 @@ const MenuItems = ({ item }) => {
         const order = products;
         order.push(item);
         setProducts(order);
-
         console.log(order)
     }
 
-    const Snacks = ({list}) => {
-        const [countSnacks, setSnacks] = useState(0);
-        
-        return (<>
-            {list.length && list.map((item) => {
-                return (<section className="menu-description" key={item.id}>
-                    <p className="product">{item.name}</p>
-                    <p className="price">${item.price}</p>
-                    <section className="input-group">
-                        <button onClick={() => setSnacks(countSnacks + 1)}> + </button>
-                        <p className="quantity-field">{countSnacks}</p>
-                        <button onClick={() => countSnacks > 0 && setSnacks(countSnacks - 1)}> - </button>
-                    </section>
+    const Snacks = ({ list }) => {
+        const [items, setItems] = useState({});
 
-                    <button className="send-button" onClick={() => { handleClick(countSnacks) }}>ADD ITEM</button>
-                </section>)
-            })}
-        </>)
+        return (
+            <section className="menu-description">
+                {list.length && list.map((item) => {
+                    let count;
+
+                    if (items[item.id])
+                        count = items[item.id]
+                    else
+                        count = 0
+
+                    return (
+                        <section className="item-description" key={item.id}>
+                            <p className="product">{item.name}</p>
+                            <p className="price">${item.price}</p>
+                            <section className="input-group">
+                                <button onClick={() => setItems({ ...items, [item.id]: count + 1 })}> + </button>
+                                <p className="quantity-field">{count}</p>
+                                <button onClick={() => count > 0 && setItems({ ...items, [item.id]: count - 1 })}> - </button>
+                            </section>
+                        </section>)
+                })}
+                <button className="send-button" onClick={() => { handleClick(items) }}>ADD ITEM</button>
+            </section>
+        )
     }
 
-    const Coffee = ({list}) => {
-        // const [countAmericano, setAmericano] = useState(0);
-        // const [countCoffee, setCoffee] = useState(0);
-        // const [countJuice, setJuice] = useState(0);
+    const Coffee = ({ list }) => {
+        const [items, setItems] = useState({});
 
-        const handleCount = (id, teste) => {
-            // console.log(count)
-            // const qtd = count + 1;
-            const newItem = {
-                id: id,
-                quantity: teste,
-            }
-            setCount([...count, newItem])
-            console.log(count)
-        }
+        return (
+            <section className="menu-description">
+                {list.length && list.map((item) => {
+                    let count;
 
-        const [count, setCount] = useState([]);
+                    if (items[item.id])
+                        count = items[item.id]
+                    else
+                        count = 0
 
-        return (<>
-            {list.length && list.map((item) => {
-                return (<section className="menu-description" key={item.id}>
-                    <p className="product">{item.name}</p>
-                    <p className="price">${item.price}</p>
-                    <section className="input-group">
-                        <button onClick={() => handleCount(item.id, (count + 1))}> + </button>
-                        <p className="quantity-field">0</p>
-                        {/* <button onClick={() => countSnacks > 0 && handleCount}> - </button> */}
-                    </section>
-
-                    <button className="send-button" onClick={(event) => { handleClick(event) }}>ADD ITEM</button> 
-                    {/* passar para o menu */}
-                </section>)
-            })}
-        </>)
+                    return (
+                        <section className="item-description" key={item.id}>
+                            <p className="product">{item.name}</p>
+                            <p className="price">${item.price}</p>
+                            <section className="input-group">
+                                <button onClick={() => setItems({ ...items, [item.id]: count + 1 })}> + </button>
+                                <p className="quantity-field">{count}</p>
+                                <button onClick={() => count > 0 && setItems({ ...items, [item.id]: count - 1 })}> - </button>
+                            </section>
+                        </section>)
+                })}
+                <button className="send-button" onClick={() => { handleClick(items) }}>ADD ITEM</button>
+            </section>
+        )
     }
 
     const Burger = () => {
@@ -162,86 +164,60 @@ const MenuItems = ({ item }) => {
         )
     }
 
-    const Sides = () => {
-        const [countFries, setFries] = useState(0);
-        const [countOnion, setOnion] = useState(0);
+    const Sides = ({ list }) => {
+        const [items, setItems] = useState({});
 
         return (
-            <section className="sides-details">
-                <section className="menu-description">
-                    <p className="product">Fries</p>
-                    <p className="price">$5</p>
-                    <section className="input-group">
-                        <button onClick={() => setFries(countFries + 1)}> + </button>
-                        <p className="quantity-field">{countFries}</p>
-                        <button onClick={() => countFries > 0 && setFries(countFries - 1)}> - </button>
-                    </section>
-                </section>
+            <section className="menu-description">
+                {list.length && list.map((item) => {
+                    let count;
 
-                <section className="menu-description">
-                    <p className="product">Onion rings</p>
-                    <p className="price">$5</p>
-                    <section className="input-group">
-                        <button onClick={() => setOnion(countOnion + 1)}> + </button>
-                        <p className="quantity-field">{countOnion}</p>
-                        <button onClick={() => countOnion > 0 && setOnion(countOnion - 1)}> - </button>
-                    </section>
-                </section>
+                    if (items[item.id])
+                        count = items[item.id]
+                    else
+                        count = 0
 
-                <button className="send-button" onClick={() => handleClick()}>ADD ITEM</button>
+                    return (
+                        <section className="item-description" key={item.id}>
+                            <p className="product">{item.name}</p>
+                            <p className="price">${item.price}</p>
+                            <section className="input-group">
+                                <button onClick={() => setItems({ ...items, [item.id]: count + 1 })}> + </button>
+                                <p className="quantity-field">{count}</p>
+                                <button onClick={() => count > 0 && setItems({ ...items, [item.id]: count - 1 })}> - </button>
+                            </section>
+                        </section>)
+                })}
+                <button className="send-button" onClick={() => { handleClick(items) }}>ADD ITEM</button>
             </section>
         )
     }
 
-    const Drinks = () => {
-        const [countWater500, setWater500] = useState(0);
-        const [countWater750, setWater750] = useState(0);
-        const [countSoda500, setSoda500] = useState(0);
-        const [countSoda750, setSoda750] = useState(0);
+    const Drinks = ({ list }) => {
+        const [items, setItems] = useState({});
 
         return (
-            <section className="drinks-details">
-                <section className="menu-description">
-                    <p className="product">Water 500ml</p>
-                    <p className="price">$5</p>
-                    <section className="input-group">
-                        <button onClick={() => setWater500(countWater500 + 1)}> + </button>
-                        <p className="quantity-field">{countWater500}</p>
-                        <button onClick={() => countWater500 > 0 && setWater500(countWater500 - 1)}> - </button>
-                    </section>
-                </section>
+            <section className="menu-description">
+                {list.length && list.map((item) => {
+                    let count;
 
-                <section className="menu-description">
-                    <p className="product">Water 750ml</p>
-                    <p className="price">$7</p>
-                    <section className="input-group">
-                        <button onClick={() => setWater750(countWater750 + 1)}> + </button>
-                        <p className="quantity-field">{countWater750}</p>
-                        <button onClick={() => countWater750 > 0 && setWater750(countWater750 - 1)}> - </button>
-                    </section>
-                </section>
+                    if (items[item.id])
+                        count = items[item.id]
+                    else
+                        count = 0
 
-                <section className="menu-description">
-                    <p className="product">Soda 500ml</p>
-                    <p className="price">$7</p>
-                    <section className="input-group">
-                        <button onClick={() => setSoda500(countSoda500 + 1)}> + </button>
-                        <p className="quantity-field">{countSoda500}</p>
-                        <button onClick={() => countSoda500 > 0 && setSoda500(countSoda500 - 1)}> - </button>
-                    </section>
-                </section>
-
-                <section className="menu-description">
-                    <p className="product">Soda 750ml</p>
-                    <p className="price">$10</p>
-                    <section className="input-group">
-                        <button onClick={() => setSoda750(countSoda750 + 1)}> + </button>
-                        <p className="quantity-field">{countSoda750}</p>
-                        <button onClick={() => countSoda750 > 0 && setSoda750(countSoda750 - 1)}> - </button>
-                    </section>
-                </section>
-
-                <button className="send-button" onClick={() => handleClick()}>ADD ITEM</button>
+                    return (
+                        <section className="item-description" key={item.id}>
+                            <p className="product">{item.name}</p>
+                            <p className="price">${item.price}</p>
+                            <section className="input-group">
+                                <button onClick={() => setItems({ ...items, [item.id]: count + 1 })}> + </button>
+                                <p className="quantity-field">{count}</p>
+                                <button onClick={() => count > 0 && setItems({ ...items, [item.id]: count - 1 })}> - </button>
+                            </section>
+                        </section>)
+                })}
+                <button className="send-button" onClick={() => { handleClick(items) }}>ADD ITEM</button>
             </section>
         )
     }
@@ -252,17 +228,19 @@ const MenuItems = ({ item }) => {
                 <Snacks list={snacksList} />
             )}
             {item === 'DrinksCoffee' && (
-                <Coffee list={coffeeList}/>
+                <Coffee list={coffeeList} />
             )}
+
+            {/* para o burger eu vou ter que dar um outro tratamento, vai ter que ser feito manualmente mesmo */}
             {item === 'Burgers' && (
                 <Burger />
             )}
 
             {item === 'Sides' && (
-                <Sides />
+                <Sides list={sidesList}/>
             )}
             {item === 'Drinks' && (
-                <Drinks />
+                <Drinks list={drinksList}/>
             )}
         </>
     )
