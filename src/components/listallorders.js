474bcec09f315281cb14e2854673e11139ffdf6c
cbalieiro <bmoliveira.camila@gmail.com>
2021-03-01 08:25:00 -0300

    realizado logica com divisoes de papeis

--- src/components/listallorders.js
+++ src/components/listallorders.js
@@ -3,34 +3,52 @@ import RequestOptions from './object/requestOptions';
 import useFetch from '../services/Hooks/useFetch';
 import CardsKitchen from './cardsKitchen';
 
-
 function ListOrders() {
   const { data, request } = useFetch();
-  // const [pending, setPending] = React.useState(null);
-  // const [done, setDone] = React.useState(null);
+  const [pending, setPending] = React.useState(null);
+  const [done, setDone] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
   const nameLS = JSON.parse(localStorage.getItem('currentUser'));
-  const { token, role } = nameLS;
+  const { token, role, id } = nameLS;
+
+  React.useEffect(() => {
+    setLoading(true);
+  }, []);
 
   React.useEffect(() => {
     async function fetchOrders() {
       const method = RequestOptions.getAndDelete('GET', token);
       const URL = 'https://lab-api-bq.herokuapp.com/orders  ';
-      const { response, json } = await request(URL, method);
+      const { json } = await request(URL, method);
       if (role === 'kitchen') {
-        console.log("Hello Kitchen");
+        const orderPending = json.filter(({ status }) => status !== 'done');
+        setPending(orderPending);
       }
-      else if (role === 'Hall') {
-        console.log("Hello Hall");
+      if (role === 'Hall') {
+        const orderDone = json.filter(
+          ({ status, uid }) => status === 'done' && uid === id,
+        );
+        setDone(orderDone);
       }
     }
+    setLoading(false);
     fetchOrders();
   }, [request, token, role]);
 
   function result() {
-    if (data) {
+    let orderlist = null;
+    if (pending) {
+      orderlist = pending;
+    }
+    if (done) {
+      orderlist = done;
+    }
+    if (orderlist) {
+      setTimeout(() => {}, 1000);
       return (
         <>
-          {data
+          {loading && <p>Carregando...</p>}
+          {orderlist
             .sort((a, b) => (a.id > b.id ? 1 : -1))
             .map((item) => {
               return (
@@ -41,9 +59,11 @@ function ListOrders() {
             })}
         </>
       );
-    } else return null;
+    } else {
+      orderlist = data;
+      return null;
+    }
   }
   return result();
 }
-
 export default ListOrders;
