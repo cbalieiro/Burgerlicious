fd9d19f49fd10abf00463638677e6505aa8e34cb
Beatriz Penalva <biapenalva@gmail.com>
2021-03-09 14:10:49 -0300

    ajustes CSS

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonSidebar from './buttonsidebar';
 import ListOrders from './listallorders';
 
-const Sidebar = ({ filterType, title }) => (
-  <section className="open">
-    <div className="overlay-dark"></div>
-    <div className={'sidebar'}>
-      <div>
-        <section>
-          <ButtonSidebar
-            id={'null'}
-            key={'button-close'}
-            className="close-button"
-          >
-            <span className="material-icons">highlight_off</span>
-          </ButtonSidebar>
-        </section>
-      </div>
-      <div className={'sidebar-container'}>
+const Sidebar = ({ filterType, title }) => {
+  const [ isOpen, setIsOpen ] = useState(false)
+  setTimeout(() => setIsOpen(true), 100)
+
+  return (
+    <section className={isOpen ? 'open' : ''}>
+      <div className="overlay-dark"></div>
+      <div className={'sidebar'}>
         <div>
-          <h2 className="title-sidebar">{title}</h2>
           <section>
-            <ListOrders filterType={filterType} />
+            <ButtonSidebar
+              id={'null'}
+              key={'button-close'}
+              className="close-button"
+            >
+              <span className="material-icons">close</span>
+            </ButtonSidebar>
           </section>
         </div>
+        <div className={'sidebar-container'}>
+          <div>
+            <h2 className="title-sidebar">{title}</h2>
+            <section>
+              <ListOrders filterType={filterType} />
+            </section>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  )
+}
 
 export default Sidebar;
