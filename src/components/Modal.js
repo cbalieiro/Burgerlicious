66d2d397889096f2f3784d0ece998bf1659810b9
Beatriz Penalva <biapenalva@gmail.com>
2021-03-11 01:11:36 -0300

    atualizando o modal

---
+++ src/components/Modal.js
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import Modal from 'react-bootstrap/Modal'
+
+const ModalMessage = (props) => (
+    <div className='back'>
+        <Modal
+            {...props}
+            size='lg'
+            aria-labelledby='contained-modal-title-vcenter'
+            centered
+        >
+            <Modal.Body>
+                <p className='modal-text'>Do you really want to delete?</p>
+                <section className="modal-bottom">
+                    <button className="modal-button yes-button" onClick={() => props.cancelOrder(true)}>YES</button>
+                    <button className="modal-button" onClick={() => props.cancelOrder(false)}>NO</button>
+                </section>
+            </Modal.Body>
+        </Modal>
+    </div>
+)
+
+export default ModalMessage
+
